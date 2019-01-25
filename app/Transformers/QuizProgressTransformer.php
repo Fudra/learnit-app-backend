501d9393024cb73a6aff15be8495dd2ca29c1a9b
Philipp <info@wahlemedia.de>
2019-01-25 20:37:46 +0100

    fix for division by 0

--- app/Transformers/QuizProgressTransformer.php
+++ app/Transformers/QuizProgressTransformer.php
@@ -18,10 +18,11 @@ class QuizProgressTransformer extends TransformerAbstract
         if ($user = auth()->user()) {
             $progressForUser = $quiz->progress()->forUser($user->id)->get()->unique('task_id');
 
+            $tasksCount = $quiz->tasks()->count();
             return [
                 'count' => $progressForUser->count(),
-                'quiz' => $quiz->tasks()->count(),
-                'percentage' => round(((float)$progressForUser->count() / (float)$quiz->tasks()->count()) * 100)
+                'quiz' => $tasksCount,
+                'percentage' => $tasksCount == 0 ? 0 : round(((float)$progressForUser->count() / (float)$tasksCount) * 100)
             ];
         }
 
